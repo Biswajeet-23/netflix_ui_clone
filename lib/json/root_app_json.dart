82ff4b5ec435f98c9d60664d553692472d88f9c0
Biswajeet Sahoo <2000biswajeet@gmail.com>
2023-11-02 09:45:30 +0530

    initial commit

---
+++ lib/json/root_app_json.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_font_icons/flutter_font_icons.dart';
+
+List items = [
+  {"icon": AntDesign.home, "text": "Home"},
+  {"icon": AntDesign.playcircleo, "text": "Coming Soon"},
+  {"icon": AntDesign.search1, "text": "Search"},
+  {"icon": AntDesign.download, "text": "Downloads"},
+];
